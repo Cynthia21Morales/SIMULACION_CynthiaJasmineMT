cced087ff4cd7f434bf8a4e9bec18e2f6e942c96
Cynthia21Morales <cielo2019mor@gmail.com>
2026-08-31 12:33:19 -0600

    Tema 2

--- SIM-Tema-2/Ejercicios/Uniformidad.R
+++ SIM-Tema-2/Ejercicios/Uniformidad.R
@@ -1,12 +1,14 @@
-# Mantener 4 intervalos
+# 1. Definir los datos (Faltaba esta parte)
+numeros <- runif(10, min = 0, max = 1)
+
+# 2. Mantener 4 intervalos
 m <- ceiling(sqrt(10))
 
-# Recalcular las frecuencias
+# 3. Recalcular las frecuencias
 breaks <- seq(0, 1, length.out = m + 1)
 histograma <- hist(numeros, breaks = breaks, plot = FALSE)
 observados <- histograma$counts
 prob_esperadas <- rep(1 / m, m)
 
-# Usar simulación
+# 4. Usar simulación
 chisq.test(x = observados, p = prob_esperadas, simulate.p.value = TRUE, B = 2000)
-
